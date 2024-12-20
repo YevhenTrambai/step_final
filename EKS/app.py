0aab74f3fc911a7a7c28307ad0554a04306e2b2d
EugenioT <eugenio.trambai@defencelab.com>
2024-12-20 21:09:13 +0100

    update app.py to show versions

--- EKS/app.py
+++ EKS/app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+VERSION = "1.0.0"
+MESSAGE = "Hello from Python App!"
+
 @app.route('/')
 def hello():
-    return '', 200
+    return f"Version: {VERSION}\n{MESSAGE}", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
